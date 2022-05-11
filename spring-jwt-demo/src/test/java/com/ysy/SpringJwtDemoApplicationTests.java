a8416f5564e98b4d191bfc6605312798d44d7cdc
yoonjungho <clubbboy@naver.com>
2022-05-11 09:43:44 +0900

    12345

--- spring-jwt-demo/src/test/java/com/ysy/SpringJwtDemoApplicationTests.java
+++ spring-jwt-demo/src/test/java/com/ysy/SpringJwtDemoApplicationTests.java
@@ -6,8 +6,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringJwtDemoApplicationTests {
 
+	
 	@Test
 	void contextLoads() {
+		
+		System.out.println("12354");
+		
+		
 	}
 
 }
